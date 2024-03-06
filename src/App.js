ebbbf7a74d66b1864714d8271acca1efbd113c9c
elyyaaa <155648362+elyyaaa@users.noreply.github.com>
2024-03-06 10:04:05 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import './App.css';
-import Modal from "./components/modal/Modal";
-import {useState} from "react";
-import List from "./components/list/List";
+import PokemonPageClass from "./components /page/pokemonPage/PokemonPageClass";
+import PokemonPage from "./components /page/pokemonPage/PokemonPage";
+import CountPageClass from "./components /page/countPageClass/CountPageClass";
+import CountPage from "./components /page/countPage/CountPage";
+import UserInfo from "./components /page/userInfo/UserInfo";
+import Users from "./components /page/users/Users";
+import FormPage from "./components /page/formPage/FormPage";
+import Menu from "./components /menu/Menu";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import PokemonInfoPage from "./components /page/pokemonInfoPage/PokemonInfoPage";
 function App() {
-    const tasks = [
-        { id: 1, title: 'coding', completed: false },
-        { id: 2, title: 'eat', completed: false },
-        { id: 3, title: 'sleep', completed: false },
-    ]
-    const [show, setShow] = useState(false)
-    console.log(show)
-    const handleShow = ()=>{
-      setShow(!show)
+     return(
+         <BrowserRouter>
+             <Menu/>
+             <Routes>
+                 <Route path='/' element={<PokemonPage/>}/>
+                 <Route path='/pokemon-class' element={<PokemonPageClass/>}/>
+                 <Route path='/count' element={<CountPage/>}/>
+                 <Route path='/countClass' element={<CountPageClass/>}/>
+                 <Route path='/users' element={<Users/>}/>
+                 <Route path='/form' element={<FormPage/>}/>
+                 <Route path='/users/:id' element={<UserInfo/>}/>
+                 <Route path='/pokemon/:id' element={<PokemonInfoPage/>}/>
+             </Routes>
 
-    }
-  return (
-    <>
-        {
-            show && <Modal handleShow={handleShow}>
-
-            </Modal>
-        }
-        <button onClick={handleShow}>open</button>
-        <h1>Task List</h1>
-        <List tasks={tasks} />
-
-    </>
-  );
+         </BrowserRouter>
+     )
 }
+
+
 
 export default App;
